73bba295b274c175e144aa0bf2808a74f4be3e31
drjlevi6 <drjlevi6@aol.com>
2021-08-11 12:40:44 -0400

    Initial commit

---
+++ config.js
@@ -0,0 +1,5 @@
+const config = {
+    CONNECTION_URI: process.env.CONNECTION_URI || 'mongodb://localhost:27017/myFlix'
+//    CONNECTION_URI: 'mongodb+srv://drjlevi6@aol.con:UcbxtwtjPeh6ZN4!@cluster0.zoljy.mongodb.net/myFlix?retryWrites=true&w=majority' || 'mongodb://localhost:27017/myFlix'
+}
+module.exports = config;
