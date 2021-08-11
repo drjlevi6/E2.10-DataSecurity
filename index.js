140ba1facf3bd9a93a6f0aedbb2da80c1a284b67
drjlevi6 <drjlevi6@aol.com>
2021-08-11 17:31:38 -0400

    Update index.js
    resumed using config.js instead of .env

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const passport = require('passport');
 require('./passport');
 
 const { check, validationResult } = require('express-validator'); // 2.10
-//const config = require('./config');
+const config = require('./config.js');
 const mongoose = require('mongoose');
 const Models = require('./models.js');
 
@@ -33,7 +33,7 @@ const Users = Models.User;
 
 //mongoose.connect('mongodb://localhost:27017/myFlix', 
 //    { useNewURLParser: true, useUnifiedTopology: true }
-mongoose.connect( '.env.CONNECTION_URI', // was config
+mongoose.connect( config.CONNECTION_URI, // was config
   { useNewURLParser: true, useUnifiedTopology: true });
 
 
