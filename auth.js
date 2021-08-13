eed8f5188ce66328dffcdb17a18785bd946e455f
drjlevi6 <drjlevi6@aol.com>
2021-08-13 19:26:21 -0400

    Update Updatre auth.js
    Try to display the login request on the web page.

--- auth.js
+++ auth.js
@@ -16,7 +16,8 @@ let generateJWTToken = (user) => {
 /* POST login: */
 module.exports = (router) => {
 	router.post('/login', (req, res) => {
-		console.log("auth.js:", req.params, req.body)
+		console.log("auth.js:", req.params, req.body);
+		return res.status(400).json(req);
 		passport.authenticate('local', { session: false }, 
 				(error, user, info) => {
 			console.log(error);
